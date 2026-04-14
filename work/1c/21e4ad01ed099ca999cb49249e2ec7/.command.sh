133fa452136c0ef9a40d1a2ae2e85907d3fcca16
Ben4586 <bengsoonccb@gmail.com>
2026-04-14 18:41:38 +0800

    Delete work directory

--- work/1c/21e4ad01ed099ca999cb49249e2ec7/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python /home/benteh/nutri-multiomics-nextflow-demo/scripts/predictive_model.py integrated.csv
--- work/df/ab0d14c4dcea5320b86ce888c82a08/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-python /home/benteh/nutri-multiomics-nextflow-demo/scripts/integrate_data.py         --micro microbiome.csv         --meta metabolomics.csv         --clinical clinical.csv         --output integrated.csv
